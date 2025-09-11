9a83fa835bc84e7ff90705367bff401e1a7233fa
chocopie <chocopie3026@naver.com>
2025-09-11 17:15:15 +0900

    조건문 완료

--- public/js/condition.js
+++ public/js/condition.js
@@ -1,15 +1,25 @@
-// let score = document.querySelector('#user_value').value;
+function calculate() {
+  let first = parseInt(document.querySelector("#user1").value);
+  let last = parseInt(document.querySelector("#user2").value);
 
-// console.log(score)
+  let opr = document.querySelector("#oper").value;
+  let result = 0;
 
-// if (score >= 90) {
-//   console.log("아주 잘했습니다");
-// } else if (score >= 80) {
-//   console.log("잘했습니다");
-// } else if (score >= 70) {
-//   console.log("보통입니다");
-// } else if (score >= 60) {
-//   console.log("노력하세요");
-// } else {
-//   console.log("망했습니다");
-// }
+  console.log(first, last, opr);
+
+  switch (opr) {
+    case '+':
+      result = first + last;
+      break;
+    case '-':
+      result = first - last;
+      break;
+    case '*':
+      result = first * last;
+      break;
+    case '/':
+      result = first / last;
+      break;
+  }
+  document.querySelector("#result").value = result;
+}
